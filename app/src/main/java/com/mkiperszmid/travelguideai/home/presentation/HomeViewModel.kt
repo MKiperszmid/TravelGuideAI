f25ab7192e7dc7726bf3ac37781cb0e7d4f037ef
Martin Kiperszmid <MKiperszmid@gmail.com>
2023-01-24 22:14:19 -0300

    Fix Viewmodel merge

--- app/src/main/java/com/mkiperszmid/travelguideai/home/presentation/HomeViewModel.kt
+++ app/src/main/java/com/mkiperszmid/travelguideai/home/presentation/HomeViewModel.kt
@@ -4,8 +4,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.mkiperszmid.travelguideai.home.domain.HomeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,12 +16,6 @@ class HomeViewModel @Inject constructor(private val repository: HomeRepository) 
     var state by mutableStateOf(HomeState())
         private set
 
-    fun onSearchTextChange(newText: String) {
-        state = state.copy(
-            searchText = newText
-        )
-    }
-
     fun search() {
         viewModelScope.launch {
             repository.getTravelGuide(state.searchText).onSuccess {
@@ -27,7 +23,7 @@ class HomeViewModel @Inject constructor(private val repository: HomeRepository) 
             }.onFailure {
                 println("Hubo un error")
             }
-        }*/
+        }
     }
 
     fun onSearchTextChange(newText: String) {
